7b8b912ff16442d0562b0c7be51d382589e4ad4a
surya-18-su <suryasahoo1868@gmail.com>
2025-09-07 18:44:54 +0530

    q1.java

---
+++ Assignment 1/q1.java
@@ -0,0 +1,10 @@
+//  Q1. Write a program to display your name, branch, roll no, and college name on the computer screen.
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Name: Suryakant sahoo");
+        System.out.println("Branch: CSE");
+        System.out.println("Roll No: 35");
+        System.out.println("College: Silicon University");
+    }
+}
